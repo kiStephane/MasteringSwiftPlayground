0c90dc962c58b9816bbf79f7a96c099bd41ada25
kiStephane <ki.stephane.dambi@gmail.com>
2017-03-14 22:16:52 +0100

    Enum and associated values discovering

--- MasteringSwiftPlayground.playground/Contents.swift
+++ MasteringSwiftPlayground.playground/Contents.swift
@@ -2,4 +2,62 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Tutorial Source http://www.appcoda.com/mastering-swift/
+// Swift enums
+
+enum DownloadStatus{
+    case downloading
+    case finished
+    case failed
+    case cancelled
+}
+
+let currentStatus = DownloadStatus.finished
+
+switch currentStatus {
+case DownloadStatus.downloading:
+    print("Downloading ...")
+    
+case DownloadStatus.finished:
+    print("Just finished the download...")
+    
+case DownloadStatus.failed:
+    print("Failed to download the file...")
+    
+case DownloadStatus.cancelled:
+    print("The download is cancelled...")
+}
+
+// Assoiated values
+enum Cloud {
+    case cirrus
+    case cumulus
+    case altocumulus
+    case stratus
+    case cumulonimbus
+}
+
+enum WeatherCondition{
+    case sunny(temperature : Float)
+    case rainy(inchesPerHour : Float)
+    case cloudy(cloudType: Cloud, windSpeed: Float)
+}
+
+let currentWeather = WeatherCondition.cloudy(cloudType: .cirrus, windSpeed: 4.2)
+
+switch currentWeather {
+case .sunny(let temperature):
+    print("It is sunny and the temperature is \(temperature)")
+case .rainy(let inchesPerHour):
+    print("It is raining at a rate of \(inchesPerHour) inches per hour.")
+    
+case .cloudy(let cloudType, let windSpeed):
+    print("It is cloudy; there are \(cloudType) clouds in the sky, and the wind speed is \(windSpeed).")
+}
+
+//--------------------------
+
+// Closures and Higher Order Functions
+
+
+
